ed39b11699e3faca9f8fad2c2fedaf4311a7b2fb
dz <eldozo1337@gmail.com>
2022-01-22 01:01:11 +0500

    second

--- hospitalnew/settings.py
+++ hospitalnew/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5dv!t-ugp*5b7e&14p-!i)xx7x92wk-#iq#1fa4^fh1*4x*-u7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
